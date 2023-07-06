ebe72282e931e1a311c01b7a1406b0befda46698
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-06 17:20:28 -0600

    Add test for node instance

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,2 +1,12 @@
 require 'rspec'
-require './lib/node'
+require './lib/node.rb'
+
+RSpec.describe Node do
+    describe "#initialize" do
+        it "is an instance of the Node class" do
+            node = Node.new("plop")
+
+            expect(node).to be_a(Node)
+        end
+    end
+end
