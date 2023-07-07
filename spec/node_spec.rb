7b83b43c58947d661bed045667c237cd7c775d02
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-06 19:25:09 -0600

    Add a test for readable attributes

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Node do
             expect(node).to be_a(Node)
         end
     end
+    describe "#data" do
+        it "can has readable attributes" do
+            node = Node.new("plop")
+
+            expect(node.data).to eq("plop")
+        end
+
+    end
 end
