3490b4f60f10236fe2be6aeceb45fea800dff2cc
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-06 20:02:30 -0600

    Add a test to verify that LinkedList is an existing class

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,3 +2,12 @@ require 'rspec'
 require './lib/linked_list.rb'
 require './lib/node.rb'
 
+RSpec.describe LinkedList do
+    describe "#initialize" do
+        it "is an instance of the LinkedList Class" do
+            list = LinkedList.new
+
+            expect(list).to be_a(LinkedList)
+        end
+    end
+end
